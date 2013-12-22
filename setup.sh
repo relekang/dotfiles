b8bae11cc369f3538528691079fcaf08f47aa619
Rolf Erik Lekang <me@rolflekang.com>
2013-12-22 09:40:04 +0100

    update paths in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 rm $HOME/.bash_profile
-ln -s $PWD/bash/bash_profile $HOME/.bash_profile
+ln -s $PWD/bash/profile $HOME/.bash_profile
 echo "Created symbolic link for .bash_profile"
 
 rm $HOME/.bash_aliases
-ln -s $PWD/bash/bash_aliases $HOME/.bash_aliases
+ln -s $PWD/bash/aliases $HOME/.bash_aliases
 echo "Created symbolic link for .bash_aliases"
 
 rm $HOME/.gitconfig
