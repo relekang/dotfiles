b79e038dae2180d6b1bde98175fc8f23da05d9d9
Rolf Erik Lekang <me@rolflekang.com>
2014-09-18 21:54:54 +0200

    start using zsh :sailboat:

--- setup.sh
+++ setup.sh
@@ -1,32 +1,6 @@
-rm $HOME/.bash_profile
-ln -s $PWD/bash/profile $HOME/.bash_profile
-echo "Created symbolic link for .bash_profile"
-
-rm $HOME/.bash_aliases
-ln -s $PWD/bash/aliases $HOME/.bash_aliases
-echo "Created symbolic link for .bash_aliases"
-
-rm $HOME/.gitconfig
-ln -s $PWD/git/gitconfig $HOME/.gitconfig
-echo "Created symbolic link for .gitconfig"
 
 if [[ $OSTYPE == "darwin12" ]]; then
   echo "Setting OS X settings"
-  sh osx_defaults.sh
+  . osx
 fi
-
-echo "Cloning vimfiles from github.com/relekang/vimfiles"
-git clone git@github.com:relekang/vimfiles.git
-
-cd vimfiles/
-git submodule init
-git submodule update
-cd ..
-
-rm $HOME/.vim
-ln -s $PWD/vimfiles/vim $HOME/.vim
-echo "Created symbolic link for .vim"
-
-rm $HOME/.vimrc
-ln -s $PWD/vimfiles/vimrc $HOME/.vimrc
-echo "Created symbolic link for .vimrc"
+. homebrew
---
+++ zshrc
@@ -0,0 +1,21 @@
+export ZSH=$HOME/.oh-my-zsh
+export DOT=$HOME/dotfiles
+
+ZSH_THEME="af-magic"
+CASE_SENSITIVE="true"
+HIST_STAMPS="yyyy-mm-dd"
+
+export PROJECTS=~/dev
+export EDITOR='vim'
+
+plugins=(autoenv git npm pip redis-cli fabric mvn bower rbates)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+export PATH="/home/rolf/apache-storm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin:/usr/texbin"
+
+. $DOT/aliases
+
+autoload -U $DOT/functions/*(:t)
