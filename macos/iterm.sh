e45155813b31e0b17989fd35c1037b1869779e30
Rolf Erik Lekang <me@rolflekang.com>
2018-12-21 09:17:04 +0100

    Set iterm window management to Advanced

--- macos/iterm.sh
+++ macos/iterm.sh
@@ -1,12 +1,12 @@
-brew cask install iterm2
+#brew cask install iterm2
 
 # Open the app so the preference files get initialized
-open -g "/Applications/iTerm.app" && sleep 2
+#open -g "/Applications/iTerm.app" && sleep 2
 
 # Disable warning when quitting
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-sleep 2 && osascript -e 'quit app "iTerm"'
+# sleep 2 && osascript -e 'quit app "iTerm"'
 
 # Fonts
 /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Normal Font' FiraCode-Regular 15" ~/Library/Preferences/com.googlecode.iTerm2.plist
@@ -23,6 +23,7 @@ sleep 2 && osascript -e 'quit app "iTerm"'
 /usr/libexec/PlistBuddy -c "Set 'DimOnlyText' 1" ~/Library/Preferences/com.googlecode.iTerm2.plist
 
 # Set new split-pane working dir to previous pane working dir
+/usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Custom Directory' Advanced" ~/Library/Preferences/com.googlecode.iTerm2.plist
 /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'AWDS Pane Option' Recycle" ~/Library/Preferences/com.googlecode.iTerm2.plist
 
 # reset the preferences cache
