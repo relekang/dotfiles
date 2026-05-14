f6bd59b18cf62af86024ae7518bb1b5c2f36fd7c
Rolf Erik Lekang <me@rolflekang.com>
2026-05-14 22:17:38 +0200

    Add jj workspace templates and bin/w

---
+++ workspace-templates/ext.sh
@@ -0,0 +1,4 @@
+w_source_repo="$HOME/dev/crdbrd/browser-extension"
+w_destination_prefix="$HOME/dev/crdbrd/ext-"
+w_tmux_project="crdbrd"
+w_env_source=".env.development"
---
+++ workspace-templates/hub.sh
@@ -0,0 +1,31 @@
+w_source_repo="$HOME/dev/crdbrd/hub"
+w_destination_prefix="$HOME/dev/crdbrd/hub-"
+w_tmux_project="hub"
+w_base_ports=(8010 8020 8030 8040 8050 8060 8070 8080 8090)
+
+w_extra_env_lines() {
+  local base_port="$1"
+  local vite_port
+
+  vite_port=$((base_port + 1))
+  if port_in_use "$vite_port"; then
+    vite_port=$((base_port + 2))
+  fi
+
+  if port_in_use "$vite_port"; then
+    echo "Error: no available VITE_PORT for base port $base_port" >&2
+    return 1
+  fi
+
+  printf 'VITE_PORT=%s\n' "$vite_port"
+}
+
+w_post_create() {
+  mise trust
+  mise run dev:setup-cert
+  ln -s "$w_source_repo/src/local_scripts" "$destination/src/local_scripts"
+  ln -s "$w_source_repo/ty.toml" "$destination/ty.toml"
+  ln -s "$w_source_repo/.plans" "$destination/.plans"
+  ln -s "$w_source_repo/.results" "$destination/.plans"
+  ln -s "$w_source_repo/.reviews" "$destination/.plans"
+}
